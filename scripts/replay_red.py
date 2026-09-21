ad837663f67f3bb50e3b52988e488d489e6cba0e
NotPellew <13049608+NotPellew@users.noreply.github.com>
2026-09-21 14:35:38 +0200

    test: replay rejected upload guidance

--- scripts/replay_red.py
+++ scripts/replay_red.py
@@ -32,6 +32,7 @@ GROUPS = (
     ("recovery_integrity_guidance", "5e9bd53", "test_cli_recovery_integrity_guidance.py", 2, {"AssertionError"}),
     ("recover_upload_integrity", "d22d75e", "test_cli_recover_upload_integrity.py", 1, {"AssertionError"}),
     ("recovery_status_lookup", "d809154", "test_cli_recovery_status_lookup.py", 1, {"AssertionError"}),
+    ("rejected_upload_guidance", "9d84cdb", "test_cli_rejected_upload_guidance.py", 1, {"AssertionError"}),
 )
 
 
