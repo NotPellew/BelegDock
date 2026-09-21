7ba5092da98b820cfae4a0c95a9db52190966e4e
NotPellew <13049608+NotPellew@users.noreply.github.com>
2026-09-21 12:33:23 +0200

    test: replay unavailable recovery commands

--- scripts/replay_red.py
+++ scripts/replay_red.py
@@ -28,6 +28,7 @@ GROUPS = (
     ("recovery_review_regressions", "2c8a7a5", "test_cli_recovery_review_regressions.py", 2, {"AssertionError"}),
     ("recovery_state_guidance", "f80559e", "test_cli_recovery_state_guidance.py", 2, {"AssertionError"}),
     ("recovery_state_unavailable", "2e17ec9", "test_cli_recovery_state_unavailable.py", 2, {"AssertionError"}),
+    ("recovery_unavailable_commands", "dc6816a", "test_cli_recovery_unavailable_commands.py", 2, {"AssertionError"}),
 )
 
 
