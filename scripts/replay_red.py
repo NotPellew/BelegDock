e0cdee4f856f588a871121319c4fe36c101797f3
NotPellew <13049608+NotPellew@users.noreply.github.com>
2026-09-24 21:09:07 +0200

    test: register path scan prefix checkpoint

--- scripts/replay_red.py
+++ scripts/replay_red.py
@@ -53,6 +53,7 @@ GROUPS = (
     ("windows_packaging_path_scan", "80dd4753bcfcd0171b6520e2e88ac19ca7c12a75", "test_windows_packaging_path_scan.py", 3, {"AssertionError"}),
     ("windows_packaging_path_scan_review", "8c6b7c17be544f1631aa25adddf61be457c41b75", "test_windows_packaging_path_scan_review.py", 2, {"AssertionError"}),
     ("windows_packaging_path_scan_boundaries", "b407b90b2ca47aa8a6087b287c3dc1c5f565354d", "test_windows_packaging_path_scan_boundaries.py", 1, {"AssertionError"}),
+    ("windows_packaging_path_scan_prefixes", "bb8db9356278a5ece97039e96e3f54e5f3747c8f", "test_windows_packaging_path_scan_prefixes.py", 1, {"AssertionError"}),
     ("desktop_launch", "29d8232bdeb2c331cdb77d4211c9ff96bf00301f", "test_desktop_launch.py", 3, {"AssertionError"}),
 )
 
