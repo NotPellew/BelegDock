4cd4d3860ff867499a1ceb1911b7ea6bee6ef6af
NotPellew <13049608+NotPellew@users.noreply.github.com>
2026-09-24 21:29:40 +0200

    test: register scan classifier RED checkpoint

--- scripts/replay_red.py
+++ scripts/replay_red.py
@@ -55,6 +55,7 @@ GROUPS = (
     ("windows_packaging_path_scan_boundaries", "b407b90b2ca47aa8a6087b287c3dc1c5f565354d", "test_windows_packaging_path_scan_boundaries.py", 1, {"AssertionError"}),
     ("windows_packaging_path_scan_prefixes", "bb8db9356278a5ece97039e96e3f54e5f3747c8f", "test_windows_packaging_path_scan_prefixes.py", 1, {"AssertionError"}),
     ("desktop_launch", "29d8232bdeb2c331cdb77d4211c9ff96bf00301f", "test_desktop_launch.py", 3, {"AssertionError"}),
+    ("scan_classification", "da035b9c7c061e77d3dedc37f8a651346cf10a45", "test_scan_classifier.py", 10, {"AssertionError"}),
 )
 
 
